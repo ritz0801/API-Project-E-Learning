fea00948f461f8b9405ec0db80a0dcdfcc8539cc
Dũng Võ <vodung0801@gmail.com>
2020-04-04 00:10:00 +0700

    init

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const { json } = require("body-parser");
 
 app.use(json());
 app.use(cors());
-
-mongoose.connect('mongodb://localhost:27017/courses', { useNewUrlParser: true, useUnifiedTopology: true })
+// mongodb+srv://abc:12345678910@course-eon1f.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect('mongodb+srv://abc:12345678910@course-eon1f.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Error" + err))
 
