f08bf8c142755ddd2c865f90be77ecfafae85613
Vo Anh Dung <vodung0801@gmail.com>
2020-04-08 00:36:58 +0700

    login and register route 1

--- module/user.js
+++ module/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     hoTen: String,
-    taiKhoan: { type: String },
+    taiKhoan: { type: String, unique: true },
     matKhau: String,
     soDienThoai: String,
     email: { type: String, unique: true },
