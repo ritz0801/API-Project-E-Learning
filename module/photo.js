a72115e36c9f60c97a0d2b9720ace8c94c6be237
Vo Anh Dung <vodung0801@gmail.com>
2020-04-27 00:30:51 +0700

    Upload Avatar

--- module/photo.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var photoSchema = new Schema({
-    path: { type: String }
-});
-module.exports = mongoose.model('Photos', photoSchema);
